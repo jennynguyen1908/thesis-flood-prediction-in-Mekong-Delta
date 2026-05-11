2392c9f22e20d57bc9bad987b372a80ab6c1b3d4
Jenny Nguyen <g.t.nguyen@tilburguniversity.edu>
2026-05-11 16:58:43 +0200

    upload code and train, validation, test data

---
+++ preprocessing/merge_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df_water = pd.read_csv("./data/04_processed/flood_data.csv")
+df_land = pd.read_csv("./data/04_processed/land_data.csv")
+
+df_water = df_water.sort_values("x_coord")
+df_land = df_land.sort_values("x_coord")
+
+# Merge water and land data
+df = pd.merge(df_water, df_land, on=["x_coord", "y_coord", "year"], how="left")
+# Create a datetime column
+df["date_time"] = pd.to_datetime(df["year"].astype(str) + df["date"].astype(str), format="%Y%j")
+df = df.sort_values("date_time")
+# Save the dataset and splitting into train, validation and test sets
+df.to_csv("./data/04_processed/flood_land_data.csv")
+# Split data into training, validation and testing set
+train_df = df[(df["year"] >= 2000) & (df["year"] < 2017)]
+test_df = df[df["year"] >= 2017]
+
+# Save as separate CSVs
+train_df.to_csv("./data/04_processed/train.csv", index=False)
+test_df.to_csv("./data/04_processed/test.csv", index=False)
